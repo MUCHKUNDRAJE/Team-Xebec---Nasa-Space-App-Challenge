85eed747d880abd0372aa2343d8c0413c023d132
MUCHKUNDRAJE <muchkundthote@gmail.com>
2025-10-04 12:18:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='h-screen w-full bg-amber-400'>
+            
+
+         
+     
+     
+      </div>
+    </>
+  )
+}
+
+export default App
