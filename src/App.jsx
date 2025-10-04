721b8a68a7a475250b4242ba69315ff7baaa7664
MUCHKUNDRAJE <muchkundthote@gmail.com>
2025-10-04 13:49:26 +0530

    added-cesium

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useEffect } from "react";
+import * as Cesium from "cesium";
+import "cesium/Build/Cesium/Widgets/widgets.css";
 
+export default function App() {
+  useEffect(() => {
+    const viewer = new Cesium.Viewer("cesiumContainer");
 
-function App() {
   
+
+
+    viewer.zoomTo(viewer.entities);
+
+  }, []);
 
   return (
     <>
-      <div className='h-screen w-full bg-amber-400'>
-            
+   <div className="h-full w-screen overflow-hidden">
+    <div id="cesiumContainer" style={{ width: "100%", height: "100vh" }}></div>
+    <div className="h-22 w-[85%] bg-white rounded-2xl p-2  border-2 border-zinc-600  shadow  absolute bottom-8 left-43  ">
+          <h1 className="alan font-bold text-2xl text-zinc-600">Solar Shied 🛡️ </h1>
+          <p className="leading-5 alan text-zinc-500 ">Solar Shield is an AI-powered platform that predicts geomagnetic storms using real NASA space-weather data and visualizes impact zones on an interactive 3D Earth map, sending timely alerts to protect critical infrastructure.</p>
 
-         
-     
-     
-      </div>
+    </div>
+   </div>
+   
+    
     </>
-  )
+  );
 }
-
-export default App
